20d6e4705ee97ff1ff7c1a1c600d97f20a1b59a2
kuvaldastalina <m.kopytenko@yandex.ru>
2024-07-25 16:57:17 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,4 @@
+my_string = input("Как тебя зовут?".upper().lower().replace(" ", '?'))
+print("Привет", my_string, "в твоём имени", len(my_string), "букв.")
+print(my_string[0])
+print(my_string[-1])
